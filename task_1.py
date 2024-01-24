c2f8cd467763077b610313940a5e94833310f60d
Serhii Klymenko <klimenko81@gmail.com>
2024-01-24 11:19:52 +0200

    Create task_1.py- first commit
    Перше завдання
    
    
    
    Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
    
    
    
    Вимоги до завдання:
    
    
    
    Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
    Функція повертає ціле число, яке вказує на кількість днів від заданої дати до поточної. Якщо задана дата пізніша за поточну, результат має бути від'ємним.
    У розрахунках необхідно враховувати лише дні, ігноруючи час (години, хвилини, секунди).
    Для роботи з датами слід використовувати модуль datetime Python.
    
    
    Рекомендації для виконання:
    
    
    
    Імпортуйте модуль datetime.
    Перетворіть рядок дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime.
    Отримайте поточну дату, використовуючи datetime.today().
    Розрахуйте різницю між поточною датою та заданою датою.
    Поверніть різницю у днях як ціле число.
    
    
    Критерії оцінювання:
    
    
    
    Коректність роботи функції: функція повинна точно обраховувати кількість днів між датами.
    Обробка винятків: функція має впоратися з неправильним форматом вхідних даних.
    Читабельність коду: код повинен бути чистим і добре документованим.
    
    
    Приклад:
    
    
    
    Якщо сьогодні 5 травня 2021 року, виклик get_days_from_today("2021-10-09") повинен повернути 157, оскільки 9 жовтня 2021 року є на 157 днів пізніше від 5 травня 2021 року.

---
+++ task_1.py
@@ -0,0 +1,29 @@
+from datetime import datetime as dt
+
+
+def get_days_from_today():
+  try:
+    # Введення користувача дати у форматі 'РРРР-ММ-ДД'
+    user_input = input("Введіть дату у форматі 'РРРР-ММ-ДД': ")
+
+    # Перетворення рядка дати у форматі 'РРРР-ММ-ДД' у об'єкт datetime
+    input_date = dt.strptime(user_input, '%Y-%m-%d')
+    #input_date = dt(2021, 5, 5)
+    # Отримання поточної дати
+    current_date = dt.today()
+    #current_date = dt(2021, 10, 9)
+
+    # Розрахунок різниці між поточною датою та введеною користувачем датою
+    difference = current_date - input_date
+
+    # Повернення різниці у днях як ціле число
+    return difference.days
+  except ValueError:
+    # Обробка винятків для неправильного формату вхідних даних
+    return "Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'."
+
+
+# Приклад виклику функції
+result = get_days_from_today()
+
+print(f"Різниця у днях: {result}")
